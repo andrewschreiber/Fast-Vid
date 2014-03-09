dce2b28616bc4e6ad69bfbe503aa5df3b3e086b5
andrewschreiber <andrew.schreiber1@gmail.com>
2014-03-08 16:58:03 -0800

    Initial Commit

---
+++ Fast Vid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fast Vid
+//
+//  Created by Andrew Schreiber on 3/8/14.
+//  Copyright (c) 2014 Andrew Schreiber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fast Vid/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fast Vid
+//
+//  Created by Andrew Schreiber on 3/8/14.
+//  Copyright (c) 2014 Andrew Schreiber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
